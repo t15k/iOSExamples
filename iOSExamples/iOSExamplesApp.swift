3290ff675df663bc64f7ffda4b30427fcb308b4b
Tonny Staunsbrink <12brink@gmail.com>
2022-08-21 12:34:54 +0200

    Initial Commit

---
+++ iOSExamples/iOSExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSExamplesApp.swift
+//  iOSExamples
+//
+//  Created by Tonny Staunsbrink on 21/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct iOSExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
